fa27619f23a6442effd5a4133049a469e01e2742
Kevin Hwang <k3vinhwang@gmail.com>
2014-10-10 23:32:54 -0700

    Add skeleton for a game page

--- server.py
+++ server.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/play")
+def play():
+    return render_template("game.html")
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
