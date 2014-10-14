650118a8580962370648839c3fcbe4154eedeacf
Kevin Hwang <k3vinhwang@gmail.com>
2014-10-13 22:13:31 -0700

    Skeleton methods

---
+++ game/characters/eligor.py
@@ -0,0 +1,3 @@
+class Eligor(Character):
+    def __init__(self):
+        self.unique_base = Aegis()
--- game/game.py
+++ game/game.py
@@ -1,2 +1,4 @@
 class Game:
     """A representation of a BattleCon game"""
+    def __init__(self, character1, character2):
+        
