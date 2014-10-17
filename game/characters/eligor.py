edb9d0628f37f4c32937dfdd0c6086e7a0a06de4
Kevin Hwang <k3vinhwang@gmail.com>
2014-10-16 19:08:51 -0700

    Get some work done mocking out Eligor

--- game/characters/eligor.py
+++ game/characters/eligor.py
@@ -26,3 +26,48 @@ class Retribution(Style):
     priority = -1
     soak = 2
 
+class Counter(Style):
+    min_range = 0
+    max_range = 0
+    power = 1
+    priority = -1
+
+    def start_of_beat(self):
+        if self.player.base.name == self.player.opponent.base.name:
+            self.player.opponent.stun()
+
+    def before_activating(self):
+        if self.player.damage_taken > 0:
+            self.player.advance(range(self.player.damage_taken + 1))
+
+class Martial(Style):
+    min_range = 0
+    max_range = 1
+    power = 1
+    priority = -1
+
+    def before_activating(self):
+        if self.player.damage_taken > 0:
+            self.player.add_power(2)
+        if self.player.tokens == 5:
+            self.player.add_power(2)
+
+class Chained(Style):
+    min_range = 0
+    max_range = 0
+    priority = -1
+
+    def before_activating(self):
+        # Pull the opponent one space per token discarded.
+
+
+class Aegis(Base):
+    min_range = 1
+    max_range = 1
+
+    @property
+    def power(self):
+        if self.player.opponent.base.printed_power is None:
+            return 0
+        else:
+            return self.opponent.style.printed_power + self.opponent.base.printed_power
